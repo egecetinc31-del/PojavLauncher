35fb018812c82169d9f11c2e136b0948bd683d75
egecetinc31-del <egecetinc31@gmail.com>
2026-05-28 17:59:20 +0000

    GLCapabilities icindeki AesirRendererInit hatasi duzeltildi

--- jre_lwjgl3glfw/src/main/java/org/lwjgl/opengl/GLCapabilities.java
+++ jre_lwjgl3glfw/src/main/java/org/lwjgl/opengl/GLCapabilities.java
@@ -4792,7 +4792,7 @@ public final class GLCapabilities {
     GLCapabilities(FunctionProvider provider, Set<String> ext, boolean fc, IntFunction<PointerBuffer> bufferFactory) {
         forwardCompatible = fc;
 
-        AesirRendererInit.onCreateCapabilities(provider);
+        PojavRendererInit.onCreateCapabilities(provider);
 
         PointerBuffer caps = bufferFactory.apply(ADDRESS_BUFFER_SIZE);
 
